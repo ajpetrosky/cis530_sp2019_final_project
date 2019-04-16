f1a2201068d4c0a7c3d7299f454e83930536ecef
MT Nahill <mtnahill@gmail.com>
2019-04-16 11:48:47 -0400

    Added HW3 function to create TF-IDF matrix

--- score.py
+++ score.py
@@ -23,6 +23,39 @@ def sil_score(input):
     print("Silhouette Score = " + str(score))
 
 
+def create_tf_idf_matrix(term_document_matrix):
+    '''Given the term document matrix, output a tf-idf weighted version.
+
+    See section 15.2.1 in the textbook.
+
+    Hint: Use numpy matrix and vector operations to speed up implementation.
+
+    Input:
+      term_document_matrix: Numpy array where each column represents a document
+      and each row, the frequency of a word in that document.
+
+    Returns:
+      A numpy array with the same dimension as term_document_matrix, where
+      A_ij is weighted by the inverse document frequency of document h.
+    '''
+    term_document_matrix[term_document_matrix > 0] = 1
+    dfs = np.sum(term_document_matrix, axis=1)
+    idfs = np.reciprocal(dfs)
+    words, docs = np.shape(term_document_matrix)
+
+    raw_idfs = np.multiply(idfs, docs)
+    idfs = np.log(raw_idfs)
+
+    tf_idf_matrix = np.zeros(np.shape(term_document_matrix))
+
+    for word in range(words):
+        for doc in range(docs):
+            tf = term_document_matrix[word, doc]
+            if tf > 0:
+                tf = 1 + np.log(tf)
+            tf_idf_matrix[word, doc] = tf * idfs[word]
+
+    return tf_idf_matrix
 
 
 def main(args):
